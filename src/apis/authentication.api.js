f3e3921b6fd4c38f45c16052461de4a4af7a81de
ITACHI UCHIHA <obito@pain.local>
2023-10-24 11:51:27 +0330

    test

--- src/apis/authentication.api.js
+++ src/apis/authentication.api.js
@@ -11,5 +11,23 @@ async function register(endpoint, data) {
   const token = await res.data
   console.log(token.data.token);
 }
+async function login(endpoint, data) {
+  console.log("data:", data);
+  const res = await axios.post(
+    `${BASE_URL}${endpoint}`,
+    data
+  );
+ 
+  console.log(data);
+}
+async function getUserIcon(endpoint, data) {
+  console.log("data:", data);
+  const res = await axios.post(
+    `${BASE_URL}${endpoint}`,
+    data
+  );
+  const token = await res.data
+  console.log(token.data.token);
+}
 
 export default register;
---
+++ src/services/LoginService.js
@@ -0,0 +1,12 @@
+import login from "../apis/authentication.api";
+
+export default async function loginUser(user) {
+  console.log(user);
+  // return login('/login' ,user);
+  console.log("wtf");
+  console.log("wtf");
+  console.log("wtf");
+  console.log("wtf");
+  console.log("wtf");
+  
+}
