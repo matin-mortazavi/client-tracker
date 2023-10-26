b5d2b21bcf9654ce652d6069aaf07c18ee539045
ITACHI UCHIHA <obito@pain.local>
2023-10-26 11:42:40 +0330

    fixing side bar

--- src/components/sidebar/action-button/FloatingActionButton.js
+++ src/components/sidebar/action-button/FloatingActionButton.js
@@ -7,64 +7,61 @@ import weight from "../../../assets/SVGs/weight-side.svg";
 import { useOverlay } from "../../../contexts/OverlayContext";
 import { createContext } from "react";
 export default function FloatingActionButton() {
-  
- 
   const [isRotated, setIsRotated] = useState(false);
   const rotatedClass = isRotated ? styles.rotated : "";
   const floatingActionHandler = () => {
-    document.body.classList = "global-overlay"
+    document.body.classList = "global-overlay";
     setIsRotated((prevState) => {
       return !prevState;
     });
- 
-    
   };
   return (
-  
-     <div
-      className={`${styles["floating-action"]} ${rotatedClass}`}
-      onClick={() => floatingActionHandler()}
-    >
-      <span
-        className={`${styles["floating-action__item"]} ${styles["floating-action__item--rotated"]}`}
+    <>
+      {isRotated && <div className={styles["backdrop"]}></div>}
+      <div
+        className={`${styles["floating-action"]} ${rotatedClass}`}
+        onClick={() => floatingActionHandler()}
       >
-        |
-      </span>
-      <span className={styles["floating-action__item"]}>|</span>
-      <div className={styles["floating-action__sub-menu"]}>
-        <div className={styles["sub-menu__container"]}>
-          <div className={styles["sub-menu__img-container"]}>
-            <img
-              className={styles["sub-menu__item-icon"]}
-              src={flash}
-              alt="not"
-            />
+        <span
+          className={`${styles["floating-action__item"]} ${styles["floating-action__item--rotated"]}`}
+        >
+          |
+        </span>
+        <span className={styles["floating-action__item"]}>|</span>
+        <div className={styles["floating-action__sub-menu"]}>
+          <div className={styles["sub-menu__container"]}>
+            <div className={styles["sub-menu__img-container"]}>
+              <img
+                className={styles["sub-menu__item-icon"]}
+                src={flash}
+                alt="not"
+              />
+            </div>
+            <p className={styles["sub-menu__item-text"]}>رویداد</p>
           </div>
-          <p className={styles["sub-menu__item-text"]}>رویداد</p>
-        </div>
-        <div className={styles["sub-menu__container"]}>
-          <div className={styles["sub-menu__img-container"]}>
-            <img
-              className={styles["sub-menu__item-icon"]}
-              src={note}
-              alt="not"
-            />
+          <div className={styles["sub-menu__container"]}>
+            <div className={styles["sub-menu__img-container"]}>
+              <img
+                className={styles["sub-menu__item-icon"]}
+                src={note}
+                alt="not"
+              />
+            </div>
+            <p className={styles["sub-menu__item-text"]}>نوع فعالیت</p>
           </div>
-          <p className={styles["sub-menu__item-text"]}>نوع فعالیت</p>
-        </div>
 
-        <div className={styles["sub-menu__container"]}>
-          <div className={styles["sub-menu__img-container"]}>
-            <img
-              className={styles["sub-menu__item-icon"]}
-              src={weight}
-              alt="not"
-            />
+          <div className={styles["sub-menu__container"]}>
+            <div className={styles["sub-menu__img-container"]}>
+              <img
+                className={styles["sub-menu__item-icon"]}
+                src={weight}
+                alt="not"
+              />
+            </div>
+            <p className={styles["sub-menu__item-text"]}>فعالیت</p>
           </div>
-          <p className={styles["sub-menu__item-text"]}>فعالیت</p>
         </div>
       </div>
-    </div>
-  
+    </>
   );
 }
