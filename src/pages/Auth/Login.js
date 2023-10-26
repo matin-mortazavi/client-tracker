a3f49e756259719760126b1bae889410be6fd974
ITACHI UCHIHA <obito@pain.local>
2023-10-26 15:07:48 +0330

    fixing password input

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,86 +1,3 @@
-// import React, { useEffect } from "react";
-// import { useForm, handleSubmit ,Controller } from "react-hook-form";
-// import styles from "./Register.module.scss";
-// import img from "../../assets/images/img.png";
-// import { Link } from "react-router-dom";
-// import axios from "axios";
-
-// export default function Register() {
-//   const {
-//     register,
-//     handleSubmit,
-//     control,
-//     formState,
-
-//     formState: { errors,isValid }} = useForm();
-//   const onSubmit = (data) => {
-//     console.log(data);
-
-//   };
-//   // const watchedValue = watch('password'); // Replace 'myInput' with your input name
-//   // const watchedValue2 = watch('email'); // Replace 'myInput' with your input name
-
-//   {
-//   }
-
-//   return (
-//     <div>
-//       <div className={styles["form-wrapper"]}>
-//         <form onSubmit={handleSubmit(onSubmit)} className={styles["form"]}>
-//           <div className={styles["img-wrapper"]}>
-//             {" "}
-//             <img className={styles["form__img"]} src={img} alt="not founded" />
-//           </div>
-//           <p className={styles["login__title"]}>ورود</p>
-
-//           <div className={styles["text-field"]}>
-
-//             <input
-//               className={styles['register__input']}
-//               {...register("password ", {
-//                 required: {
-//                   value: true,
-//                   message: "wtf",
-//                 },
-//                 minLength: { value: 4, message: "min" },
-//               })}
-//               type="password"
-//             />
-
-//              <label htmlFor="password">رمز عبور</label>
-//           </div>
-//           <div className={styles["text-field"]}>
-
-//             <input
-//               className={styles["register__input"]}
-//               {...register("password ", {
-//                 required: {
-//                   value: true,
-//                   message: "wtf",
-//                 },
-//                 minLength: { value: 4, message: "min" },
-//               })}
-//               type="password"
-//             />
-//              <label htmlFor="password">رمز عبور</label>
-//           </div>
-//           {errors.password?.type === "required" && <p>form-box</p>}
-
-//           <button className={styles["register__btn"]} type="submit">
-//             ورود
-//           </button>
-//           <p className={styles["register__text"]}>
-//             اکانت دارید؟!{" "}
-//             <Link className={styles["register__link"]} to={"#"}>
-//               وارد شوید
-//             </Link>
-//           </p>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// }
-
 import React from "react";
 import { useForm, Controller } from "react-hook-form";
 import { Link, redirect, useNavigate } from "react-router-dom";
@@ -90,6 +7,8 @@ import user from "../../assets/SVGs/user-icon.svg";
 import lock from "../../assets/SVGs/lock.png";
 import { useState } from "react";
 import eye from "../../assets/SVGs/eye-slash.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import loginUser from "../../services/LoginService";
 
 function RegisterPage() {
@@ -99,7 +18,9 @@ function RegisterPage() {
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    mode: 'onBlur', // Set the mode to 'onBlur'
+  });
   console.log(errors);
   const handlePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
@@ -107,97 +28,110 @@ function RegisterPage() {
   };
 
   const onSubmit = (data) => {
-    loginUser(data)
+    loginUser(data);
     console.log(data);
-    
   };
 
   return (
     <div className={styles["form-wrapper"]}>
-       
       <div className={styles["form-box"]}>
-      <div className={styles.form}>
-        <img className={styles["form__img"]} src={img} alt="not founded" />
-        <h2 className={styles["form__title"]}>ورود</h2>
-        <form className={styles["form__content"]}  onSubmit={handleSubmit(onSubmit)}>
-          <div
-            className={
-              watch("email")
-                ? `${styles.notEmpty} ${styles.inputField}`
-                : `${styles.inputField}`
-            }
+        <div className={styles.form}>
+          <img className={styles["form__img"]} src={img} alt="not founded" />
+          <h2 className={styles["form__title"]}>ورود</h2>
+          <form
+            className={styles["form__content"]}
+            onSubmit={handleSubmit(onSubmit)}
           >
-            <Controller
-              name="email"
-              control={control}
-              rules={{ required: "Email is required" }}
-              defaultValue={""}
-              render={({ field }) => (
-                <>
-                  {" "}
-                  <img className={styles["form__icon"]} src={user} alt="" />
-                  <input
-                    className={styles["form__input"]}
-                    {...field}
-                    type="email"
-                  />
-                </>
-              )}
-            />
-            <label>ایمیل</label>
-            {errors.email && (
-              <p className={styles.errorText}>{errors.email.message}</p>
-            )}
-          </div>
+            <div
+              className={
+                watch("email")
+                  ? `${styles.notEmpty} ${styles.inputField}`
+                  : `${styles.inputField}`
+              }
+            >
+              <Controller
+                name="email"
+                control={control}
+                rules={{ required: "Email is required" }}
+                defaultValue={""}
+                render={({ field }) => (
+                  <>
+                    {" "}
+                    <img className={styles["form__icon"]} src={user} alt="" />
+                    <input
+                      className={styles["form__input"]}
+                      {...field}
+                      type="email"
+                    />
+                  </>
+                )}
+              />
+              <label>ایمیل</label>
 
-          <div
-            className={`${styles.inputField} ${
-              watch("password")
-                ? `${styles.notEmpty} ${styles.inputField}`
-                : `${styles.inputField}`
-            }`}
-          >
-            <Controller
-              name="password"
-              control={control}
-              rules={{ required: "Password is required" }}
-              defaultValue={""}
-              render={({ field }) => (
-                <>
-                  <img className={styles["form__icon"]} src={lock} alt="" />
+              {errors.email &&
+                
+                  <ToastContainer
+                  position="bottom-right"
+                  autoClose={5000}
+                  hideProgressBar={false}
+                  newestOnTop={false}
+                  closeOnClick
+                  rtl={false}
+                  pauseOnFocusLoss
+                  draggable
+                  pauseOnHover
+                  theme="light"
+                />
+              }
+            </div>
 
-                  <input
-                    className={styles["form__input"]}
-                    {...field}
-                    type={passwordVisible ? "text" : "password"}
-                  />
-                  <img
-                    onClick={handlePasswordVisibility}
-                    className={` ${styles["password-icon"]}`}
-                    src={eye}
-                    alt=""
-                  />
-                </>
-              )}
-            />
-            <label>رمز عبور</label>
-            {errors?.password && (
-              <p className={styles.errorText}>{errors.password.message}</p>
-            )}
-          </div>
+            <div
+              className={
+                watch("password")
+                  ? `${styles.notEmpty} ${styles.inputField}`
+                  : `${styles.inputField}`
+              }
+            >
+              <Controller
+                name="password"
+                control={control}
+                rules={{ required: "Password is required" }}
+                defaultValue={""}
+                render={({ field }) => (
+                  <>
+                    {" "}
+                    <img className={styles["form__icon"]} src={user} alt="" />
+                    <input
+                      className={styles["form__input"]}
+                      {...field}
+                      type={passwordVisible ? "text" : "password"}
+                    />
+                  </>
+                )}
+              />
+              <label>پسورد</label>
+              <img
+                
+                className={styles["password-icon"]}
+                src={eye}
+                alt=""
+                onClick={handlePasswordVisibility}
+              ></img>
+              
+            </div>
 
-          <button className={styles["form__btn"]} type="submit">
-            ورود
-          </button>
-          <span className={styles["form__text"]}>
-            {" "}
-            حساب کاربری ندارید؟{" "}
-            <Link className={styles["form__link"]} to={"/register"}>
-              ثبت نام
-            </Link>
-          </span>
-        </form>
-      </div>
+            <button className={styles["form__btn"]} type="submit">
+              ورود
+            </button>
+            <span className={styles["form__text"]}>
+              {" "}
+              حساب کاربری ندارید؟{" "}
+              <Link className={styles["form__link"]} to={"/register"}>
+                ثبت نام
+              </Link>
+            </span>
+          </form>
+        </div>
       </div>
     </div>
   );
