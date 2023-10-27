7206802600d58a63076b1c907859b54aa8d52038
ITACHI UCHIHA <obito@pain.local>
2023-10-28 00:13:22 +0330

    edit some folders

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import styles from "./Dashboard.module.scss";
 
-import SideBar from "../../components/sidebar/Sidebar";
-import Reports from "../../components/reports/Reports";
-import Events from "../../components/events/Events";
-import Activities from "../../components/activities/Activities"
+import SideBar from "../../components/Common/sidebar/Sidebar";
+import Reports from "../../components/dashboard/reports/Reports";
+import Events from "../../components/dashboard/events/Events";
+import Activities from "../../components/dashboard/activities/Activities"
 import User from "../../components/User/User";
-import ActivityCategory from "../../components/activityCategory/ActivitiyCategory"
+import ActivityCategory from "../../components/dashboard/activityCategory/ActivitiyCategory"
 
 export default function Dashboard() {
   return (
