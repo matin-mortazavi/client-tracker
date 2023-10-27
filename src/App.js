12587c4802bde9133337655b10b1c19fd6cafdc0
ITACHI UCHIHA <obito@pain.local>
2023-10-27 11:39:58 +0330

    add user info section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from "./logo.svg";
 import "./App.scss";
+
 import { useRoutes } from "react-router-dom";
 import appRouter from "./routes/appRouter";
+
 // import { OverlayProvider } from "../../my-app/src/contexts/OverlayContext";
 // import { useOverlay } from "../../my-app/src/contexts/OverlayContext";
 function App() {
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -2,20 +2,26 @@ import React from 'react'
 import { useState } from 'react';
 import "react-modern-calendar-datepicker/lib/DatePicker.css";
 import { Calendar } from "react-modern-calendar-datepicker";
+import UserEvents from "../userEvents/UserEvents";
+import "./styles.scss";
+import UserInfo from '../userInfo/UserInfo';
 
-import "./styles.scss"
+
 export default function User() {
 const dropDowm = document.querySelectorAll("div")
     console.log(dropDowm);
     const [selectedDay, setSelectedDay] = useState(null);
   return (
-    <div className='idk'> 
+    <div className='user-section'> 
+    <UserInfo></UserInfo>
        <Calendar
       value={selectedDay}
       onChange={setSelectedDay}
       shouldHighlightWeekends
       locale="fa" // add this
     />
+    <UserEvents></UserEvents>
+    
         
     </div>
   )
